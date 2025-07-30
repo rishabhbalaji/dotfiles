a268d335d9c287ebafc8a9f2baac100c8c192d67
Rishabh Balaji K <rbk1357246@gmail.com>
2025-07-30 04:23:12 +0100

    Dotfile Update

--- polybar_launch.sh
+++ polybar_launch.sh
@@ -6,7 +6,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config.ini
-polybar -c ~/.config/polybar/config.ini main &
+# Get monitor names
+# This command uses xrandr to list connected monitors, then cuts out their names
+# and filters out disconnected/off entries.
+MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
 
-echo "Polybar launched..."
+# Launch Polybar for each detected monitor
+for m in $MONITORS; do
+  if [ "$m" = "HDMI-1" ]; then # Ensure "HDMI-1" is correct
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini primary &
+  else
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini secondary & # <--- This is the crucial line for eDP-1
+  fi
+done
+
+echo "Polybar launched on all monitors..."
