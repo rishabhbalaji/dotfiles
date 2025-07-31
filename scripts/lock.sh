704caf37c8e3ec16ffd81730e49774ece0c8d4a1
Rishabh Balaji K <rbk1357246@gmail.com>
2025-07-31 04:26:54 +0100

    Updated  Dotfiles

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
-# Custom lock script for dynamic, styled i3lock-color screen
-# Takes current screenshot, updates betterlockscreen cache, then locks.
+# Simple lock script for instant locking using betterlockscreen's cached image
 
-IMG="/tmp/current_lock_screen_bg.png" # Temporary screenshot file
-
-# 1. Take a screenshot of the current screen
-# Adjust -q 75 for quality/speed tradeoff
-scrot -z -q 25 "$IMG"
-
-# 2. Update betterlockscreen's cache with the new screenshot
-# This will apply blur, dim, and other effects defined in ~/.config/betterlockscreen/betterlockscreenrc
-betterlockscreen -u "$IMG" --fx
-
-# 3. Lock the screen with betterlockscreen (which uses the newly cached image)
 betterlockscreen -l
-
-# 4. Clean up the temporary screenshot
-rm "$IMG"
 
 exit 0
